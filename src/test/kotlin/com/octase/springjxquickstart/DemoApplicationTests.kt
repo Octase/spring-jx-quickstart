289d6e00514f78c3d28dc95ab50a23438bc3098e
Francis Laforge <francis@flaforge.fr>
2020-04-25 14:46:26 +0200

    add quote feat and test

--- src/test/kotlin/com/octase/springjxquickstart/DemoApplicationTests.kt
+++ src/test/kotlin/com/octase/springjxquickstart/DemoApplicationTests.kt
@@ -1,13 +1,25 @@
 package com.octase.springjxquickstart
 
+import org.hibernate.validator.internal.util.Contracts.assertNotNull
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Autowired
+    lateinit var testRestTemplate: TestRestTemplate
+
+    @Test
+    fun testHelloController() {
+        val result = testRestTemplate.getForEntity("/hello", String::class.java)
+        assertNotNull(result)
+        assertEquals(result.statusCode, HttpStatus.OK)
+        assertEquals(result.body, "World")
+    }
 
 }
